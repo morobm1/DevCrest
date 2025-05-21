0b3ea2f48652f284cb20feec907e16742659d811
morobm1 <104599805+morobm1@users.noreply.github.com>
2025-05-21 02:27:15 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProjectSelector from "./ProjectSelector";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>Welcome to DevCrest</h1>
+      <p>This is the homepage. Go to <a href="/master">/master</a> to view projects.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <ProjectSelector />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/master" element={<ProjectSelector />} />
+      </Routes>
+    </Router>
   );
 }
 
