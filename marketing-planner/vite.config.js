87711738f75cdd5106fda605f9c3a15e8865a1ed
morobm1 <104599805+morobm1@users.noreply.github.com>
2025-05-29 18:09:30 -0600

    update

--- marketing-planner/vite.config.js
+++ marketing-planner/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/marketing-planner/',
   root: '.',
   plugins: [react()],
   server: {
